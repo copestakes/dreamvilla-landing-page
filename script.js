496b910033cea15a7e4c615701eb593d62f7c8eb
copestakes <davecopestakes@gmail.com>
2023-04-05 18:37:55 -0400

    update testimonials

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function checkAll() {
 window.onload = function () {
   window.addEventListener('resize', checkAll, false);
 };
+
+// const checkbox = document.querySelector('#check');
+// // add a click event listener to the window object
+// window.addEventListener('click', function (event) {
+//   // if the clicked element is not the checkbox
+//   if (event.target !== checkbox) {
+//     // uncheck the checkbox
+//     checkbox.checked = false;
+//   }
+// });
