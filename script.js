44bb8ce5971a2235d31c72dbf40e4339b70a6b21
copestakes <davecopestakes@gmail.com>
2023-04-18 22:51:51 -0400

    updated navbar

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+// function checkAll() {
+//   const input = document.querySelector('#check');
+//   input.checked = false;
+// }
+// window.onload = function () {
+//   window.addEventListener('resize', checkAll, false);
+// };
+
+// window.addEventListener('click', function () {
+//   var checkbox = document.querySelector('#check');
+//   checkbox.checked = !checkbox.checked;
+// });
+
+//new navigation
+const input = document.querySelector('#check');
+const nav = document.querySelector('nav ul');
+const anchors = document.querySelectorAll('a');
+
 function checkAll() {
-  const input = document.querySelector('#check');
   input.checked = false;
 }
 window.onload = function () {
   window.addEventListener('resize', checkAll, false);
+  nav.addEventListener('click', checkAll, false);
 };
 
-window.addEventListener('click', function () {
-  var checkbox = document.querySelector('#check');
-  checkbox.checked = !checkbox.checked;
+anchors.forEach((e) => {
+  e.addEventListener('click', checkAll, false);
 });
 
 // const checkbox = document.querySelector('#check');
